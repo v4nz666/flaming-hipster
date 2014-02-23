91aec5889fbd2f4d8191a8ce8ccd468cff506e0d
Jeff Ripley <666v4nz@gmail.com>
2014-02-23 15:56:52 -0400

    Cleanup - remove debugerry

--- display.py
+++ display.py
@@ -11,32 +11,26 @@ class Display :
     libtcod.console_set_default_foreground(0, libtcod.white)
     libtcod.console_init_root(width, height, b'title', False, libtcod.RENDERER_GLSL)
     
-    #self._selection = libtcod.console_new(1,1)
-    #libtcod.console_set_default_background(self._selection, libtcod.green)
-    #libtcod.console_set_background_flag(self._selection, libtcod.BKGND_LIGHTEN)
-    
 
   def render(self, gui) :
     
     cells = gui.board.getCells()
     selected = gui.getSelected()
     
-    print "SEL: ", selected
-    
+    # Draw each cell...
     for c in cells:
+      # 3 Horizontal screen poisitions, per cell
       for _x in range(3) :
-        # Our actual position on the screen
+        
+        # Our actual position on the screen, offset by 1 for the frame...
         y = 1 + (c.y)
         x = 1 + (3 * c.x + _x)
         libtcod.console_set_char_background(0, x, y, getattr(libtcod, c.color))
         
+        # If we're rendering the selected cell, add our selector's color
         if selected[0] == c.x and selected[1] == c.y :
-          print "Rendering selection at: ", c.x, c.y
           libtcod.console_set_char_background(0, x, y, libtcod.green, libtcod.BKGND_ADDALPHA(0.4))
-          #libtcod.console_blit(self._selection, 0,0, 1,1, 0, x,y)
-          
     
-    
-    
+    # finally, flush the console...
     libtcod.console_flush()
 
